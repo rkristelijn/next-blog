e8321a0adf012656e9921c41708eb5a5212537ed
remi.kristelijn <remi.kristelijn@theapsgroup.com>
2025-07-21 15:12:14 +0200

    cleanup: remove unused CSS module files after MUI migration

--- src/components/ThemeRegistry.tsx
+++ src/components/ThemeRegistry.tsx
@@ -5,7 +5,7 @@ import { useServerInsertedHTML } from 'next/navigation';
 import { CacheProvider } from '@emotion/react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/lib/theme';
+import theme from '../lib/theme';
 import { useState } from 'react';
 
 // This implementation is from emotion-js
